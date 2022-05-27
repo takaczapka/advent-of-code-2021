6590f2acd90f72fb7e6025d0131025121dad3cc8
Maciej Falski <maciej@hi55.uk>
2022-05-27 16:35:13 +0100

    Tidy up day 5

--- src/advent_of_code_2021/5.clj
+++ src/advent_of_code_2021/5.clj
@@ -1,22 +1,13 @@
 (ns advent-of-code-2021.5
-  (:require
-    [clojure.test :refer :all]
-    [clojure.java.io :as io]
-    [clojure.string :as str]
-    [clojure.pprint :refer [pprint]]))
-
-(defn read-input [file]
-  (->
-    (io/resource file)
-    (slurp)
-    (str/split #"\n")))
+  (:require [clojure.test :refer :all]
+            [clojure.string :as str]
+            [clojure.pprint :refer [pprint]]
+            [advent-of-code-2021.common :as common]))
 
 (defn parse-line [l]
-  (prn l)
   (let [[a b] (str/split l #" -> ")]
-    [(map #(Integer/parseInt %) (str/split a #","))
-     (map #(Integer/parseInt %) (str/split b #","))])
-  )
+    [(map #(parse-long %) (str/split a #","))
+     (map #(parse-long %) (str/split b #","))]))
 
 (defn parse-input [is]
   (map parse-line is))
@@ -41,23 +32,17 @@
             (and (< x x') (< y y')) (take-while #(not= p2 %) (iterate (fn [[a b]] [(inc a) (inc b)]) p1))
             (and (< x x') (> y y')) (take-while #(not= p2 %) (iterate (fn [[a b]] [(inc a) (dec b)]) p1))
             (and (> x x') (< y y')) (take-while #(not= p2 %) (iterate (fn [[a b]] [(dec a) (inc b)]) p1))
-            (and (> x x') (> y y')) (take-while #(not= p2 %) (iterate (fn [[a b]] [(dec a) (dec b)]) p1))
-
-            )))
-  )
+            (and (> x x') (> y y')) (take-while #(not= p2 %) (iterate (fn [[a b]] [(dec a) (dec b)]) p1))))))
 
 (defn draw [pnts]
   (let [board (vec (repeat 10 (vec (repeat 10 0))))]
     (reduce (fn [bs [a b]] (assoc-in bs [b a] 6))
-            board pnts))
-  )
+            board pnts)))
 
 (defn count-intersections [cords]
   (count (filter #(< 1 (count %)) (vals (group-by identity (map vec (apply concat (map #(points (first %) (second %)) cords))))))))
 
-(deftest asdasds-test
-
-
+(deftest aoc-5-test
   (is (= (set [[1 1]]) (set (points [1 1] [1 1]))))
 
   (is (= (set [[1 1] [1 2]]) (set (points [1 1] [1 2]))))
@@ -66,27 +51,12 @@
 
   (is (= (set [[2 1] [3 1] [4 1] [5 1]]) (set (points [2 1] [5 1]))))
 
-  (is (= (set [[0 9] [1 9] [2 9] [3 9] [4 9] [5 9]]) (set (points [0,9] [5,9]))))
+  (is (= (set [[0 9] [1 9] [2 9] [3 9] [4 9] [5 9]]) (set (points [0, 9] [5, 9]))))
 
-  (is (= (set [[9,7,] [8,8] [7,9]]) (set (points [9 7] [7 9]))))
+  (is (= (set [[9, 7,] [8, 8] [7, 9]]) (set (points [9 7] [7 9]))))
 
   (is (= (set [[1 3] [2 3] [3 3]]) (set (points [1 3] [3 3]))))
 
-
-  (let [
-        input    (str/split "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2" #"\n")
-        input (read-input "5.txt")
-        _ (prn input)
-        cords    (parse-input input)
-        _ (prn cords)
-        pointszz (map #(points (first %) (second %)) cords)
-        ;_        (prn pointszz)
-        ;_        (prn (map vec (apply concat pointszz)))
-        ]
-    (prn (count-intersections cords))
-    ;(prn (count (filter #(< 1 (count %)) (vals (group-by identity (map vec (apply concat pointszz)))))))
-    ;
-    ;(pprint (draw (apply concat pointszz)))
-    )
-
-  )
+  (let [input    (common/read-input "5.txt")
+        cords    (parse-input input)]
+    (is (= 17882 (count-intersections cords)))))
