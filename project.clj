f2995b5c4c61996554af6401bd1c23c01523dcba
Maciej Falski <maciej@hi55.uk>
2022-05-27 15:25:16 +0100

    Tidy up

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject advent-of-code-2021 "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.11.0"]]
-  :main ^:skip-aot advent-of-code-2021.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot      :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
