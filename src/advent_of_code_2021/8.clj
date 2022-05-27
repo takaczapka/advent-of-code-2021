822b23d24e054c71ff7ecf69f07bf06bf2f8d503
Maciej Falski <maciej@hi55.uk>
2022-05-27 17:12:46 +0100

    Tidy up day 8

--- src/advent_of_code_2021/8.clj
+++ src/advent_of_code_2021/8.clj
@@ -1,46 +1,28 @@
 (ns advent-of-code-2021.8
   (:require [clojure.test :refer :all]
             [clojure.string :as str]
-            [clojure.set :as set]
             [clojure.java.io :as io]))
 
-
-
-
-
-
-
-
-
-
-
-(defn do-the-mapping [i p-mapping]
-  (map (fn [p] (map (fn [v] (replace p v)) i)) p-mapping))
-
-
-(def valid (set (to-dd "abcefg cf acdeg acdeg acdfg bcdf abdfg acf abcdefg abcdfg")))
-
-(defn is-valid [c]
-  (prn (set c))
-  ;(prn "valid" valid)
-  (set/subset? (set c) valid)
-  )
+(defn sort-string [s]
+  (str/join (sort s)))
 
 (defn read-input [i]
   (let [lines (str/split i #"\n")]
-    (map #(map (fn [s] (str/split s #" ")) (str/split % #" \| ")) lines)
-    )
-  )
-
+    (map
+      #(map (fn [s]
+              (map
+                sort-string
+                (str/split s #" ")))
+            (str/split % #" \| "))
+      lines)))
 
 (defn common-with [s1 s2]
   (count (filter #(.contains s1 (str %)) (vec s2))))
 
-
-(defn aaaa [ss]
-  (let [one  (first (filter #(= 2 (count %)) ss))
-        four (first (filter #(= 4 (count %)) ss))
-        a    (map (fn [s] [s (count s) (common-with s one) (common-with s four)]) ss)]
+(defn classify [ss]
+  (let [one        (first (filter #(= 2 (count %)) ss))
+        four       (first (filter #(= 4 (count %)) ss))
+        normalised (map (fn [s] [s (count s) (common-with s one) (common-with s four)]) ss)]
     (into {} (map (fn [[s & rest]]
                     [s (cond
                          (= [2 2 2] rest) 1
@@ -53,42 +35,30 @@
                          (= [7 2 4] rest) 8
                          (= [6 2 4] rest) 9
                          (= [6 2 3] rest) 0
-                         :default :YYY
-                         )]) a))))
-
-(defn sort-string [s]
-  (str/join (sort s)))
-
-(defn bbbb [ss to-enc]
-  (prn "vvvv" (aaaa (map sort-string ss)))
-  (prn "vvvv" (sort-string to-enc))
-
-  (get (aaaa (map sort-string ss)) (sort-string to-enc))
-
-  )
+                         :default :YYY)])
+                  normalised))))
 
 (deftest aoc-8
 
   ; part 1
-  ;(let [in "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
-  ;
-  ;      ]
-  ;
-  ;  (let [res (map count (flatten (read-input (slurp (io/resource "8.txt")))))]
-  ;    ;(prn res)
-  ;    (prn (count (filter #(or (= % 2) (= % 4) (= % 3) (= % 7)) res)))
-  ;    ))
-
-  (prn (read-input (slurp (io/resource "8.txt"))))
+  (let [res
+        (->>
+          "8.txt"
+          io/resource
+          slurp
+          read-input
+          (map second)
+          flatten
+          (map count))]
+    (is (= 416 (count (filter #(or (= % 2) (= % 4) (= % 3) (= % 7)) res)))))
 
   ; part 2
   (let [in (read-input (slurp (io/resource "8.txt")))]
-    (prn (apply + (map (fn [[left right]] (let [m (aaaa (map sort-string left))
-                                  ]
-                                   (Integer/parseInt (str/join (map #(get m (sort-string %)) right)))
-                              )) in)))
-
-
-    )
-
-  )
+    (is (= 1043697
+           (apply +
+                  (map
+                    (fn [[left right]]
+                      (let [m (classify left)]
+                        (parse-long (str/join (map #(get m %) right)))))
+                    in))))
+    ))
